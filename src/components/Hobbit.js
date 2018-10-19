4f287c935e4318af9f1ef73dd70b77d6b10537c0
perpepajn25 <perpepajn25@gmail.com>
2018-10-19 17:06:11 -0400

    unused variables part 2

--- src/components/Hobbit.js
+++ src/components/Hobbit.js
@@ -5,13 +5,6 @@ import { connect } from 'react-redux'
 const Hobbit = props => {
   const { hobbit } = props
   const selected = false
-  let stock
-  if (false) {
-    stock = 'something'
-  } else {
-    stock = 'something else'
-  }
-
   return (
     <div className='card' style={selected ? {'borderColor':'cyan'} : {'borderColor':'red'} } onClick={() => console.log(hobbit)}>
       <img alt={hobbit.name} src={hobbit.image_url} />
@@ -22,7 +15,6 @@ const Hobbit = props => {
       <h3> Position: {hobbit.position} </h3>
       <h3> Key Skill: {hobbit.key_skill} </h3>
       <h3> Employment Type: {hobbit.employment_type} </h3>
-      {stock}
     </div>
   );
 };
