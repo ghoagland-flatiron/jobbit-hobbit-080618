cf3921f7af55d34e00423224b27b0d2d665ef168
ghoagland-flatiron <georgina.hoagland@flatironschool.com>
2018-12-12 13:33:09 -0500

    prep for lecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import HobbitContainer from './HobbitContainer'
+import HogContainer from './HogContainer'
 
 class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <HobbitContainer />
-      </div>
+      <Router>
+        <div className='App'>
+          <Route path='/hobbits' component={HobbitContainer} />
+          <Route path='/hogs' component={HogContainer} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/data/porkers_data.js
@@ -0,0 +1,95 @@
+const hogs = [
+  {
+    name: 'Mudblood',
+    specialty: 'Mediocre magic',
+    greased: false,
+    weight_ratio: 2.0,
+    highest_medal_achieved: 'bronze'
+  },
+  {
+    name: 'Porkchop',
+    specialty: 'Making friends',
+    greased: true,
+    weight_ratio: 1.6,
+    highest_medal_achieved: 'silver'
+  },
+  {
+    name: 'Cherub',
+    specialty: 'truffle finder',
+    greased: false,
+    weight_ratio: 0.7,
+    highest_medal_achieved: 'gold'
+  },
+  {
+    name: 'Piggy smalls',
+    specialty: 'Massivity',
+    greased: true,
+    weight_ratio: 5.1,
+    highest_medal_achieved: 'platinum'
+  },
+  {
+    name: 'Trouble',
+    specialty: 'Racing',
+    greased: true,
+    weight_ratio: 1.7,
+    highest_medal_achieved: 'gold'
+  },
+  {
+    name: 'Sobriety',
+    specialty: 'Expert Witness',
+    greased: false,
+    weight_ratio: 2.2,
+    highest_medal_achieved: 'silver'
+  },
+  {
+    name: 'Rainbowdash',
+    specialty: 'Union Busting',
+    greased: false,
+    weight_ratio: 3.7,
+    highest_medal_achieved: 'wood'
+  },
+  {
+    name: 'TruffleShuffle',
+    specialty: 'Finding truffles',
+    greased: true,
+    weight_ratio: 4.0,
+    highest_medal_achieved: 'gold'
+  },
+  {
+    name: 'Bay of Pigs',
+    specialty: 'Hamphlet Distribution',
+    greased: false,
+    weight_ratio: 2.8,
+    highest_medal_achieved: 'diamond'
+  },
+  {
+    name: 'The Prosciutto Concern',
+    specialty: 'truffle finder',
+    greased: false,
+    weight_ratio: 2.3,
+    highest_medal_achieved: 'bronze'
+  },
+  {
+    name: 'Galaxy Note',
+    specialty: 'Airport Security',
+    greased: true,
+    weight_ratio: 1.9,
+    highest_medal_achieved: 'diamond'
+  },
+  {
+    name: 'Leggo My Eggo',
+    specialty: 'Babysitting',
+    greased: true,
+    weight_ratio: 3.3,
+    highest_medal_achieved: 'platinum'
+  },
+  {
+    name: 'Augustus Gloop',
+    specialty: 'Vacuum tubes',
+    greased: true,
+    weight_ratio: 4.1,
+    highest_medal_achieved: 'wood'
+  }
+];
+
+export default hogs;
